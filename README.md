adad519c5052c69eeb51ed4f18ba2dee4e5bc469
JobanD <joban.d555@gmail.com>
2021-08-07 14:04:17 -0400

    Added readme and other files to hide secret key

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fantasy Basketball Project V2.0
+
+Creating a web based fantasy basketball project using django. Updated version of old project containing new features and better utilizing the django framework.
--- fantasy_basketball/settings.py
+++ fantasy_basketball/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n7in513q(ay3egwsuc@)kq9a&3$#%+-@g(w(_v83^cs3!lioxd'
+
+SECRET_KEY = config('SECRET_KEY') # So that secret key is not visible 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
