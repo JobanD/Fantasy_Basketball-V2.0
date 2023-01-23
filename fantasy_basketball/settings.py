6adf20159eb611a479697c6416ab1725b2e6e6c7
JobanD <joban.d555@gmail.com>
2023-01-22 20:05:19 -0500

    Added requirements.txt

--- fantasy_basketball/settings.py
+++ fantasy_basketball/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'teams/static/teams'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
