f4e7f89d7a456bc6e9c62ec91273f65a72940557
JobanD <joban.d555@gmail.com>
2023-01-22 20:12:25 -0500

    settings.py change for config

--- fantasy_basketball/settings.py
+++ fantasy_basketball/settings.py
@@ -136,13 +136,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'main/static/main'),
     os.path.join(BASE_DIR, 'players/static/players'),
     # os.path.join(BASE_DIR, 'teams/static/teams'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
